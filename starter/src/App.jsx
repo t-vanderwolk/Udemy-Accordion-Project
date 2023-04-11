864a6cc272ab2b6dbd302078bc9d73fb82732c1e
Taylor Vanderwolk <taylorvanderwolk@Taylors-MBP.lan>
2023-04-11 12:01:39 -0700

    Questions - Alternative Setup

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -3,10 +3,21 @@ import data from './data';
 import Questions from './Questions';
 
 const App = () => {
- const [questions, setQuestions] = useState(data)
+  const [activeId, setActiveId]=useState(null);
+ const [questions, setQuestions] = useState(data);
+
+const toggleQuestion = (id) => {
+  const newActiveId = id === activeId? null : id;
+  setActiveId(newActiveId)
+}
+
+
   return (
     <main>
-    <Questions questions={questions} />
+    <Questions 
+    questions={questions} 
+    activeId={activeId} 
+    toggleQuestion={toggleQuestion} />
     </main>
   );
 };
